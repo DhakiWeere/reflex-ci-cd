02ff7d0ae3643ecf98a0d5537a5dd048a7a29b20
DhakiWeere <dhanikaweerasekara@outlook.com>
2025-10-12 04:21:20 +0530

    next router setup | monaco editor setup

---
+++ reflex-web/src/app/api/push-code/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    console.log("route GET called")
+    return NextResponse.json({ message: 'Hello from API!' });
+}
+
+export async function POST(req) {
+    console.log("route POST called");
+    const headers = req.headers;
+    const content_type = await headers.get("content-type");
+    
+    const data =  await req.json(); 
+    console.log(data);
+    console.log(content_type);
+    console.log(data.username + " <<<>>> " + data.pass);
+    return NextResponse.json({ name : "THis is awesome" });
+}
--- reflex-web/src/app/page.jsx
+++ reflex-web/src/app/page.jsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import Editor from "@monaco-editor/react";
+import './editor.css';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>App Router</h1>
-      <h3>SPA Reflect CD/CD</h3>
+  const [code, setCode] = useState("// Start typing code here...");
 
-    </div>
+  return (
+    <main className="main-container">
+      <h1 className="page-title">React Code Editor</h1>
+      <div className="editor-wrapper">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          value={code}
+          theme="vs-dark"
+          onChange={(value) => setCode(value || "")}
+        />
+      </div>
+      {/* <pre className="code-output">{code}</pre> */}
+      <button className="test-btn" 
+      onClick={() => {alert(`CODE : \n ${code}`)}}>Get Code</button>
+    </main>
   );
 }
