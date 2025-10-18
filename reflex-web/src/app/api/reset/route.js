8de6b46944eb24a071119799ab758a33252e2647
DhakiWeere <dhanikaweerasekara@outlook.com>
2025-10-18 22:01:50 +0530

    reset api route remove

--- reflex-web/src/app/api/reset/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { configDotenv } from 'dotenv';
-import { Octokit } from '@octokit/rest';
-
-export async function POST(req) {
-    const reqBodyObj = await req.json()
-    
-}
